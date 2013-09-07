8aa8281b5f17f272d9294602a9f04f853ec65540
johnernaut <john@johnjohnson.cc>
2013-09-07 17:39:23 -0500

    json requests

--- battlefury.go
+++ battlefury.go
@@ -1,19 +1,19 @@
 package main
 
 import (
-    "github.com/johnernaut/battlefury/router"
-    "log"
-    "net/http"
+	"github.com/johnernaut/battlefury/router"
+	"log"
+	"net/http"
 )
 
 func main() {
-    r, err := router.BuildRoutes()
+	r, err := router.BuildRoutes()
 
-    if err != nil {
-        log.Panic(err)
-    }
+	if err != nil {
+		log.Panic(err)
+	}
 
-    http.Handle("/", r)
-    log.Println("Listening on port 8005")
-    log.Fatal(http.ListenAndServe(":8005", http.DefaultServeMux))
+	http.Handle("/", r)
+	log.Println("Listening on port 8005")
+	log.Fatal(http.ListenAndServe(":8005", http.DefaultServeMux))
 }
--- router/routes.go
+++ router/routes.go
@@ -1,18 +1,19 @@
 package router
 
 import (
-    "fmt"
-    "net/http"
-    "github.com/gorilla/mux"
+	"fmt"
+	"github.com/gorilla/mux"
+	"net/http"
 )
 
 func BuildRoutes() (*mux.Router, error) {
-    r := mux.NewRouter()
-    r.HandleFunc("/", indexHandler)
+	r := mux.NewRouter()
+	r.Headers("Content-Type", "application/json")
+	r.HandleFunc("/", indexHandler)
 
-    return r, nil
+	return r, nil
 }
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "I love %s!", r.URL.Path[1:])
+	fmt.Fprintf(w, "I love %s!", r.URL.Path[1:])
 }

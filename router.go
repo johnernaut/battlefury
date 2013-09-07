09ca71d4f35576a211034565fcc187fb51f3658d
johnernaut <john@johnjohnson.cc>
2013-09-07 16:25:09 -0500

    fc

---
+++ router.go
@@ -0,0 +1,18 @@
+package battlefury
+
+import (
+    "fmt"
+    "net/http"
+    "github.com/gorilla/mux"
+)
+
+func BuildRoutes() (*mux.Router, error) {
+    r := mux.NewRouter()
+    r.HandleFunc("/", indexHandler)
+
+    return r, nil
+}
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "I love %s!", r.URL.Path[1:])
+}
